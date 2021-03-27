99495285f446421240aa5c2ff1a949fa2bb92855
Josip Rezić <josip@feelmorelabs.com>
2021-03-28 00:53:26 +0100

    Add project

---
+++ moozeek-swiftui/moozeek_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  moozeek_swiftuiApp.swift
+//  moozeek-swiftui
+//
+//  Created by Josip Rezić on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct moozeek_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
