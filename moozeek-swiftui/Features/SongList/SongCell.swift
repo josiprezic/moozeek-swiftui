181ffa7d4eaedbf00a59585ba8e6c7906d3fdf3b
Josip Rezić <josip.rezic@hotmail.com>
2022-11-09 23:15:52 +0100

    Update SongCell UI

--- moozeek-swiftui/Features/SongList/SongCell.swift
+++ moozeek-swiftui/Features/SongList/SongCell.swift
@@ -11,7 +11,46 @@ struct SongCell: View {
     let song: Song
     
     var body: some View {
-        Text(song.name)
+        HStack {
+            musicNote
+            Text(song.name)
+            Spacer()
+            downloadButton
+            menuButton
+        }
+        .padding(-5)
+    }
+    
+    var musicNote: some View {
+        Image(systemName: "music.note")
+            .buttonStyle(PlainButtonStyle())
+            .frame(width: 50, height: 50)
+            .background(Color.white.opacity(0.17))
+            .foregroundColor(Color.white.opacity(0.4))
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.white.opacity(0.3))
+            )
+            .padding(.trailing, 12)
+    }
+    
+    var downloadButton: some View {
+        Button(action: {},
+               label: {
+            Image(systemName: "arrow.down.circle.fill")
+                .resizable()
+                .frame(width: 12, height: 12)
+                .opacity(0.3)
+        })
+    }
+    
+    var menuButton: some View {
+        Button(
+            action: {},
+            label: {
+                Image(systemName: "ellipsis")
+            }
+        )
     }
 }
 
