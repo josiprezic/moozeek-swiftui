19183793dba9110111d873accab131002d853838
Josip Rezić <josip.rezic@hotmail.com>
2023-01-14 13:44:38 +0100

    Add LocalBroadcasters section in RadioView

--- moozeek-swiftui/Features/Radio/RadioView.swift
+++ moozeek-swiftui/Features/Radio/RadioView.swift
@@ -13,16 +13,24 @@ struct RadioView: View {
             VStack {
                 if Reachability.isConnected {
                     ScrollView {
-                        ScrollView(.horizontal) {
-                            HStack {
-                                radioHCollectionView
-                                radioHCollectionView
-                                radioHCollectionView
-                                radioHCollectionView
+                        VStack(alignment: .leading, spacing: 24) {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    radioHCollectionViewItem
+                                    radioHCollectionViewItem
+                                    radioHCollectionViewItem
+                                    radioHCollectionViewItem
+                                }
                             }
-                            
+                            localBroadcastersTitle
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    radioListHCollectionViewList
+                                    radioListHCollectionViewList
+                                    radioListHCollectionViewList
+                                }
+                            }
                         }
-//                        Text("Radio View")
                     }
                 } else {
                     YouAreOfflineView()
@@ -32,20 +40,61 @@ struct RadioView: View {
         }
     }
     
-    private var radioHCollectionView: some View {
+    private var radioHCollectionViewItem: some View {
         VStack(alignment: .leading) {
-            Text("Exclusive")
-                .font(.callout)
+            Text("Exclusive".uppercased())
+                .font(.caption2)
+                .opacity(0.5)
             Text("Apple Music 1")
                 .font(.headline)
             Text("The new music that matters.")
                 .font(.headline)
+                .fontWeight(.regular)
+                .opacity(0.5)
             Image("gnr_logo")
                 .resizable()
                 .scaledToFill()
-                .frame(width: UIScreen.main.bounds.width - 50, height: 300)
+                .frame(width: UIScreen.main.bounds.width - 30, height: 300)
                 .cornerRadius(10)
         }
+    }
+    
+    private var localBroadcastersTitle: some View {
+        Text("Local Broadcasters")
+            .font(.title2)
+            .fontWeight(.bold)
+    }
+    
+    private var radioListHCollectionViewList: some View {
+        VStack {
+            radioListHCollectionViewItem
+            radioListHCollectionViewItem
+            radioListHCollectionViewItem
+        }
+    }
+    
+    private var radioListHCollectionViewItem: some View {
+        HStack {
+            Image("gnr_logo")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 100)
+                .cornerRadius(10)
+            VStack(alignment: .leading) {
+                Text("Zeta FM")
+                Text("From TuneIn")
+                    .font(.caption2)
+                    .opacity(0.5)
+            }
+            Spacer()
+            Button(action: {}) {
+                Image(systemName: Style.Image.more)
+                    .renderingMode(.template)
+                    .foregroundColor(.primary)
+                    .frame(width: 30, height: 30)
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width - 30)
     }
 }
 
