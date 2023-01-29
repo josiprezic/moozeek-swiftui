b97de16cb8eea516d0978b9d1d5ba5aafc4db6f7
Josip Rezić <josip.rezic@hotmail.com>
2023-01-29 20:47:13 +0100

    Add Radio model

---
+++ moozeek-swiftui/Features/Radio/Radio.swift
@@ -0,0 +1,14 @@
+//
+//  Radio.swift
+//  moozeek-swiftui
+//
+//  Created by Josip Rezic on 29/01/2023.
+//
+
+import Foundation
+
+struct Radio {
+    let name: String = "Radio name"
+    let description = "From TuneIn"
+    let logo: String = "gnr_logo"
+}
