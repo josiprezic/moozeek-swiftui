ffbc332dfe546041a84c83dcabb41a2cbfe3d967
Josip Rezić <josip.rezic@hotmail.com>
2023-01-30 21:20:30 +0100

    Update RadioViewModel, add exclusive, local and international section models

--- moozeek-swiftui/Features/Radio/RadioViewModel.swift
+++ moozeek-swiftui/Features/Radio/RadioViewModel.swift
@@ -12,4 +12,16 @@ final class RadioViewModel: ViewModel, ObservableObject {
     var isOnline: Bool {
         Reachability.isConnected
     }
+    
+    var exclusiveSection: [Radio] {
+        Array(repeating: Radio(), count: 9)
+    }
+    
+    var localBroadcasters: [Radio] {
+        Array(repeating: Radio(), count: 9)
+    }
+    
+    var internationalBroadcasters: [Radio] {
+        Array(repeating: Radio(), count: 9)
+    }
 }
