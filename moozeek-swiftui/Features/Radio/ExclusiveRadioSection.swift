66807c277108f164fee059a25dec3efb7841b2be
Josip Rezić <josip.rezic@hotmail.com>
2023-02-07 20:40:26 +0100

    Move ExclusiveRadioSection to separate file

--- moozeek-swiftui/Features/Radio/ExclusiveRadioSection.swift
+++ moozeek-swiftui/Features/Radio/ExclusiveRadioSection.swift
@@ -5,4 +5,37 @@
 //  Created by Josip Rezic on 07/02/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct ExclusiveRadioSection: View {
+    var offset: CGFloat = 15.0
+    let radioList: [Radio]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                Spacer().frame(width: offset)
+                ForEach(radioList, content: exclusiveView)
+            }
+        }
+    }
+    
+    private func exclusiveView(for radio: Radio) -> some View {
+        VStack(alignment: .leading) {
+            Text("Exclusive".uppercased())
+                .font(.caption2)
+                .opacity(0.5)
+            Text(radio.name)
+                .font(.headline)
+            Text(radio.description)
+                .font(.headline)
+                .fontWeight(.regular)
+                .opacity(0.5)
+            Image(radio.logo)
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width - 30, height: 300)
+                .cornerRadius(10)
+        }
+    }
+}
--- moozeek-swiftui/Features/Radio/RadioView.swift
+++ moozeek-swiftui/Features/Radio/RadioView.swift
@@ -146,36 +146,3 @@ struct RadioView_Previews: PreviewProvider {
         RadioView.resolved
     }
 }
-
-struct ExclusiveRadioSection: View {
-    var offset: CGFloat = 15.0
-    let radioList: [Radio]
-    
-    var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                Spacer().frame(width: offset)
-                ForEach(radioList, content: exclusiveView)
-            }
-        }
-    }
-    
-    private func exclusiveView(for radio: Radio) -> some View {
-        VStack(alignment: .leading) {
-            Text("Exclusive".uppercased())
-                .font(.caption2)
-                .opacity(0.5)
-            Text(radio.name)
-                .font(.headline)
-            Text(radio.description)
-                .font(.headline)
-                .fontWeight(.regular)
-                .opacity(0.5)
-            Image(radio.logo)
-                .resizable()
-                .scaledToFill()
-                .frame(width: UIScreen.main.bounds.width - 30, height: 300)
-                .cornerRadius(10)
-        }
-    }
-}
