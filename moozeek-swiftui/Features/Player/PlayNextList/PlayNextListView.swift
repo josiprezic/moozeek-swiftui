95916e64699cba28cfe13f2631f5d1547a65261a
Josip Rezić <josip.rezic@hotmail.com>
2023-02-14 23:07:20 +0100

    Add PlayNextListView

---
+++ moozeek-swiftui/Features/Player/PlayNextList/PlayNextListView.swift
@@ -0,0 +1,14 @@
+//
+//  PlayNextListView.swift
+//  moozeek-swiftui
+//
+//  Created by Josip Rezic on 14/02/2023.
+//
+
+import SwiftUI
+
+struct PlayNextListView: View {
+    var body: some View {
+        Text("PlayNextListView")
+    }
+}
