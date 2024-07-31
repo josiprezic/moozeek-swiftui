dbe8cf7121cd281cea15853f767de777515bae00
Josip Rezić <josip.rezic@hotmail.com>
2024-07-31 18:15:14 +0200

    doc: SearchViewModelDelegate

--- moozeek-swiftui/Features/Search/SearchViewModel.swift
+++ moozeek-swiftui/Features/Search/SearchViewModel.swift
@@ -7,15 +7,21 @@
 
 import UIKit
 
+/// Protocol that defines the delegate methods for `SearchViewModel`
 protocol SearchViewModelDelegate: AnyObject {
+    /// Called when a song has been downloaded
     func didDownloadSong()
 }
 
+/// A view model class for managing the state and actions of the search view
 final class SearchViewModel: ViewModel, ObservableObject {
+    /// The download manager responsible for handling song downloads
     private let downloadManager: DownloadManager
     
+    /// The title text displayed in the search view
     let titleText = "Instructions"
     
+    /// The description text displayed in the search view
     let descriptionText =
     """
     1. Go to [www.youtube.com](https://youtube.com)
@@ -27,12 +33,18 @@ final class SearchViewModel: ViewModel, ObservableObject {
     7. Enjoy!
     """
     
+    /// Initializes a new instance of `SearchViewModel`
+    /// - Parameter downloadManager: The download manager responsible for handling song downloads
     init(downloadManager: DownloadManager) {
         self.downloadManager = downloadManager
     }
     
+    /// Handles the action when the paste button is selected
     func handlePasteSelected() {
+        // Retrieve the content from the pasteboard
         let pasteboardContent = UIPasteboard.general.string ?? ""
+        
+        // Use the download manager to download the song from the provided URL
         downloadManager.downloadYouTubeSong(from: pasteboardContent)
     }
 }
