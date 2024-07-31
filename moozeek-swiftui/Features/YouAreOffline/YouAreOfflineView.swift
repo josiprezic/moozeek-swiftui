9530a3371f55aa98923ce0aa5fe3f32059a8b87e
Josip Rezić <josip.rezic@hotmail.com>
2024-07-31 18:11:50 +0200

    doc: YouAreOfflineView

--- moozeek-swiftui/Features/YouAreOffline/YouAreOfflineView.swift
+++ moozeek-swiftui/Features/YouAreOffline/YouAreOfflineView.swift
@@ -7,19 +7,29 @@
 
 import SwiftUI
 
+import SwiftUI
+
+/// A view that displays an offline message to the user.
 struct YouAreOfflineView: View {
+    /// The body of the `YouAreOfflineView` which contains the user interface elements.
     var body: some View {
         VStack {
+            // The main title text that informs the user they are offline.
             Text("You're Offline")
                 .font(.title.bold())
+            // A secondary message suggesting the user turn off Airplane Mode or connect to Wi-Fi.
             Text("Turn off Airplane Mode or connect to Wi-Fi")
                 .font(.body)
         }
+        .padding()
+        .multilineTextAlignment(.center)
     }
 }
 
+/// A preview provider for `YouAreOfflineView` to enable live preview in Xcode.
 struct YouAreOfflineView_Previews: PreviewProvider {
+    /// Previews the `YouAreOfflineView` for SwiftUI canvas.
     static var previews: some View {
-        YouAreOfflineView.resolved
+        YouAreOfflineView()
     }
 }
