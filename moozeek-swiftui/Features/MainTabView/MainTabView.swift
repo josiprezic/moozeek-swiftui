4f9f9ab2ed413c5b6c3cc834f59463c1ee6e5713
Josip Rezić <josip.rezic@hotmail.com>
2024-08-03 11:18:21 +0200

    chore: update preview code

--- moozeek-swiftui/Features/MainTabView/MainTabView.swift
+++ moozeek-swiftui/Features/MainTabView/MainTabView.swift
@@ -33,10 +33,8 @@ struct MainTabView: View {
     }
 }
 
-struct MainTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainTabView.resolved
-    }
+#Preview {
+    MainTabView.resolved
 }
 
 
